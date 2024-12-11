1f96b85c6581b3c4dcccb74e3bfff8ae35dd289b
Fardin-Git <fardin.kenari@gmail.com>
2024-12-11 16:34:34 +0100

    Uebung8_1V2

--- src/org/hbrs/se1/ws24/exercises/uebung8/Hotelsuche.java
+++ src/org/hbrs/se1/ws24/exercises/uebung8/Hotelsuche.java
@@ -2,7 +2,12 @@ package org.hbrs.se1.ws24.exercises.uebung8;
 
 public class Hotelsuche {
     newHotelsuche search = new Adapter();
-    public Suchergebnis suchen(SuchAuftrag s){
-        return search.suche(s);
+    public Suchergebnis suche(String s){
+        SuchAuftrag s2= sucheEingeben(s);
+        return search.suche(s2);
     }
+    public SuchAuftrag sucheEingeben(String s){
+        return new SuchAuftrag();
+    }
+
 }
